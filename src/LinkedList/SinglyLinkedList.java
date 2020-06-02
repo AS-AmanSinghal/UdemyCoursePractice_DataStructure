59246e569ef139558ca33de0c7f93ef41f0a5f23
Aman Singhal <amansinghal.it1@gmail.com>
2020-06-03 00:20:47 +0530

    Singly Linked List

---
+++ src/LinkedList/SinglyLinkedList.java
@@ -0,0 +1,19 @@
+package LinkedList;
+
+class Node
+{
+    int value;
+    Node next;
+
+    public Node(int value) {
+        this.value = value;}
+}
+
+public class SinglyLinkedList
+{
+
+    public void addAtBegining(int value)
+    {
+        
+    }
+}
